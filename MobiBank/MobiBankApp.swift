1cb4713246cf68704f73647a16c17dfd6a3a81a9
kutasiva <kutasov@seznam.cz>
2024-12-23 11:50:56 +0100

    Initial Commit

---
+++ MobiBank/MobiBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobiBankApp.swift
+//  MobiBank
+//
+//  Created by kutasov on 23.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct MobiBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
