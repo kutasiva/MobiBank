b3e9111e599f5533fcfc035fe8906581cc2e933e
kutasiva <kutasov@seznam.cz>
2024-12-23 12:10:43 +0100

    feat: [001] Add skeleton app

--- MobiBank/ContentView.swift
+++ MobiBank/ContentView.swift
@@ -7,18 +7,65 @@
 
 import SwiftUI
 
+protocol DataServiceProtocol {
+    func getTitle() async -> String
+}
+
+actor DataSerivce: DataServiceProtocol {
+    func getTitle() async -> String {
+        try? await Task.sleep(for: .seconds(1))
+        return "Updated title"
+    }
+}
+
+actor MockDataService: DataServiceProtocol {
+    let testData: String
+
+    init(data: String? = nil) {
+        self.testData = data ?? "testData"
+    }
+
+    func getTitle() async -> String {
+        try? await Task.sleep(for: .seconds(1))
+        return "Mock title"
+    }
+}
+
+@MainActor
+@Observable class BankAccountViewModel {
+    private let dataSerivce: DataServiceProtocol
+
+    var title: String = "Starting title"
+
+    init(dataSerivce: DataServiceProtocol) {
+        self.dataSerivce = dataSerivce
+    }
+
+    func updateTitle() async {
+        title = await dataSerivce.getTitle()
+    }
+}
+
 struct ContentView: View {
+    let viewModel: BankAccountViewModel
+
+    init(dataSerivce: DataServiceProtocol) {
+        self.viewModel = BankAccountViewModel(dataSerivce: dataSerivce)
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(viewModel.title)
+                .task {
+                    await viewModel.updateTitle()
+                }
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    let viewModel = DataSerivce()
+    let mock = MockDataService(data: "Mock data")
+    ContentView(dataSerivce: mock)
 }
